e205b4cb11b29c509c6f4a90bd21639679414295
Juliette <julietteredheadgirl@yahoo.com>
2019-05-03 16:19:59 -0500

    update tar v4.4.8 + npm audit fix again

--- server/server.js
+++ server/server.js
@@ -14,6 +14,7 @@ const app = express();
 mongoose.connect(process.env.DATABASE, {useMongoClient: true}, err => {
   if (err) {
     console.log(err);
+    console.log(err);
   } else {
     console.log('Connected to the database');
   }
